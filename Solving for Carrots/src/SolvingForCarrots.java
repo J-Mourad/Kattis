b63051720d055f49667b58ec12f71efea8e7a23c
Jmourad <jbenemourad@gmail.com>
2017-11-12 17:03:19 +0000

    Commited SolvingForCarrots

---
+++ Solving for Carrots/src/SolvingForCarrots.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class SolvingForCarrots {
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+        int n = sn.nextInt();  // n doesn't matter.. nor does following lines
+        int p = sn.nextInt(); 
+        
+        System.out.println(p);
+    }
+}
