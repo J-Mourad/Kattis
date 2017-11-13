b958d37faec628d96e03836c623248f8d664fb7e
Jmourad <jbenemourad@gmail.com>
2017-11-13 17:05:24 +0000

    Commited Good Solution using the StringBuilder Class

--- Backspace/src/Backspace.java
+++ Backspace/src/Backspace.java
@@ -1,32 +1,22 @@
 import java.io.BufferedInputStream;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class Backspace {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(new BufferedInputStream(System.in));
-		ArrayList<Character> Tab = new ArrayList<Character>();
-		String text = sc.nextLine();
-		int t=0;
-		for(int i = text.length()-1; i>=0 ; i--) {
-			
-			if(text.charAt(i) ==  '<') {
-				t++;
-				continue;
-			}
-			i = i - t;
-			if(text.charAt(i) !=  '<') {
-				Tab.add(text.charAt(i));
+		String line = sc.nextLine();
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<line.length(); i++) {
+			char c = line.charAt(i);
+			if(c == '<') {
+				if(sb.length() > 0) {
+					sb.deleteCharAt(sb.length()-1);
+				}
 			}else {
-				i--;
+				sb.append(c);
 			}
-			t=0;
 		}
-		Collections.reverse(Tab);
-		for (Character character : Tab) {
-			System.out.print(character);
-		}
-		sc.close();
+		System.out.println(sb);
 	}
 }
