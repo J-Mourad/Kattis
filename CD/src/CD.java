5840ced2fe696c518d81f31467f49500cbe5489d
Jmourad <jbenemourad@gmail.com>
2017-11-19 03:06:17 +0000

    Commited Time Limit Exceeded

--- CD/src/CD.java
+++ CD/src/CD.java
@@ -2,30 +2,39 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CD {
+	
+	static Scanner scan = new Scanner(System.in);
+	static ArrayList<Integer> cdsJack = new ArrayList<Integer>();
+	static ArrayList<Integer> cdsJill = new ArrayList<Integer>();
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
 
 		while (true) {
 			int n = scan.nextInt();
 			int m = scan.nextInt();
-
 			if (n == 0 && m == 0)
 				break;
 
-			ArrayList<Integer> cds = new ArrayList<Integer>();
-
-			int count = 0;
-
 			for (int i = 0; i < n; i++)
-				cds.add(scan.nextInt());
-
+				cdsJack.add(scan.nextInt());
+			
 			for (int i = 0; i < m; i++)
-				if (cds.contains(scan.nextInt()))
-					count++;
+				cdsJill.add(scan.nextInt());
 
-			System.out.println(count);
+	        int count = 0, ni = 0, mi = 0;
+
+            while (ni < n && mi < m) {
+                if (cdsJack.get(ni) > cdsJill.get(mi)) {
+                    mi++;
+                } else if (cdsJack.get(ni) < cdsJill.get(mi)) {
+                    ni++;
+                } else {
+                    count++;
+                    ni++;
+                    mi++;
+                }
+            }
+            System.out.println(count);
 		}
-
 		scan.close();
 	}
 }
